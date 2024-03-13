68f425a2e08c3f22c798ba061e1c61d421bb125b
aniketshelke8010 <aniketshelke9900@gmail.com>
2024-03-13 22:25:39 +0530

    headettop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { CiSearch } from "react-icons/ci";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App () {
+    return (
+        <div className='bg-light'>
+            <header>
+                <div className='a_top bg-white py-1'>
+                    <div style={{width:"84%"}} className="container">
+                        <div className="row w-100 justify-content-between mx-auto">
+                            <div className="col-12 col-md-7">
+                                <div className="row align-items-center">
+                                    <div className="col-12 col-md-3">
+                                        <div className="logo">
+                                            <img height={65} src="https://getlogo.net/wp-content/uploads/2020/04/bookmyshow-logo-vector.png" alt="" />
+                                        </div>
+                                    </div>
+                                    <div className="col-12 col-md-9">
+                                        <div className="d-flex align-items-center p-1 px-2 gap-2 border justify-content-start rounded-2">
+                                            <CiSearch />
+                                            <input type="text" className="border-0 w-100" placeholder="Search for Movies,Events,Plays,Sports and Activities" style={{flex:1,outline:"none",fontSize:"14px"}}/>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-md-3 bg-danger">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='a_bottom'>
+                    Bottom
+                </div>
+            </header>
+        </div>
+    )
 }
 
-export default App;
+export default App
